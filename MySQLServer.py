b890e315230d176fc2420abc1d51c29e6b649e04
Eric Hervinn <hervinnpo@gmail.com>
2025-06-29 23:37:33 +0200

    adding changes mysqlserver file

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,7 +4,6 @@ Script to create the 'alx_book_store' database
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -20,14 +19,14 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        if connection.is_connected():
+        if 'connection' in locals() and connection.is_connected():
             cursor.close()
             connection.close()
-            # Optional: uncomment to see this confirmation
+            # Optional: uncomment to confirm closing
             # print("MySQL connection is closed.")
 
 if __name__ == "__main__":
